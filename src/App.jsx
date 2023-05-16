5047c1acb8a957309b37ef8461dc3aeca39b8f51
Ashade Samson <ashadesamson99@gmail.com>
2023-05-16 09:44:26 +0100

    updated the verify email and change email fuctionality

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,7 @@ function App() {
               <Route path="messages" element={<Messages />} />
               <Route path="billing" element={<Billing />} />
               <Route path="resources" element={<Resources />} />
-              <Route path="settings" element={<Settings />} >
-                <Route path="setname" element={<h1>change name</h1>}/>
-                <Route path="setmail" element={<h1>Change Email</h1>}/>
-                <Route path="verifymail" element={<h1>Verify Email</h1>}/>
-                <Route path="setpassword" element={<h1>change Password</h1>}/>
-              </Route>
+              <Route path="settings" element={<Settings />} />
             </Route>
             <Route path="booking" element={<Booking/>} action={bookingAction}/>
           </Route>
