d83545c6ca0dbfe51410c9c4e7b55d658122b49a
Ashade Samson <ashadesamson99@gmail.com>
2024-11-11 08:09:25 +0100

    [style]: Add sub components and styles for landing page redesign

---
+++ src/components/landingPage/faqs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../../styles/landingPage/faqs.module.css'
+
+function Faqs() {
+  return (
+    <div>Faqs</div>
+  )
+}
+
+export default Faqs
---
+++ src/components/landingPage/therapists.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../../styles/landingPage/therapists.module.css'
+
+function Therapists() {
+  return (
+    <div>Therapists</div>
+  )
+}
+
+export default Therapists
