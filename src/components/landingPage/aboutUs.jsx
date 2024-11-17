fc4a6766dcb6cc39855438a87601343fba9172ca
Ashade Samson <ashadesamson99@gmail.com>
2024-11-17 12:41:01 +0100

    [style]: Adjust button opacity for mobile screens

--- src/components/landingPage/aboutUs.jsx
+++ src/components/landingPage/aboutUs.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
 import styles from '../../assets/styles/landingPage/aboutUs.module.css'
 import { text } from '../../services/fake-data/data';
+import { animated } from '@react-spring/web';
+import { useAnimation } from '../../hooks/useAnimation';
 
 
 function AboutUs() {
+
+  // Animations details
+  const [ref, animation] = useAnimation(
+    { opacity: 0, transform: 'rotate(20deg)' },
+    { opacity: 1, transform: 'rotate(0deg)' }
+  );
 
   const [isExpanded, setIsExpanded] = useState(false);
   const previewText = text.slice(0, 500);
@@ -12,21 +20,23 @@ function AboutUs() {
 
 
   return (
-    <div className={styles.aboutContainer}>
-      <h2 className={styles.heading}>About Us</h2>
-      <div className={styles.text}>
-        {isExpanded
-          ? paragraphs.map((paragraph, index) => (
-              <p key={index} className={styles.paragraph}>
-                {paragraph}
-              </p>
-            ))
-          : `${previewText}...`}
+    <animated.section ref={ref} style={animation} id="about">
+      <div className={styles.aboutContainer}>
+        <h2 className={styles.heading}>About Us</h2>
+        <div className={styles.text}>
+          {isExpanded
+            ? paragraphs.map((paragraph, index) => (
+                <p key={index} className={styles.paragraph}>
+                  {paragraph}
+                </p>
+              ))
+            : `${previewText}...`}
+        </div>
+        <button className={styles.toggleButton} onClick={() => setIsExpanded(!isExpanded)}>
+          {isExpanded ? 'Read Less' : 'Read More'}
+        </button>
       </div>
-      <button className={styles.toggleButton} onClick={() => setIsExpanded(!isExpanded)}>
-        {isExpanded ? 'Read Less' : 'Read More'}
-      </button>
-    </div>
+    </animated.section>
   )
 }
 
