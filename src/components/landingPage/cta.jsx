e1a4ac988c57d376a95553bda54b036af1499ffd
Ashade Samson <ashadesamson99@gmail.com>
2024-11-21 12:06:44 +0100

    [style]: Update animations on landing page components

--- src/components/landingPage/cta.jsx
+++ src/components/landingPage/cta.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from '../../assets/styles/landingPage/cta.module.css'
 import ctaImg from '../../assets/images/cta.webp'
+import Animated from '../animations/animated'
 
 function CTA({user}) {
   return (
     <div className={styles.ctaContainer}>
     <div className={styles.ctaImage}>
-      <img src={ctaImg} alt="Call to Action" />
+      <Animated className={styles.ctaImage}>
+        <img src={ctaImg} alt="Call to Action" />
+      </Animated>
     </div>
     <div className={styles.ctaText}>
-      <h1 className={styles.ctaHeading}>Unlock Your Potential, Design Your Future</h1>
-      <div className={styles.ctaParagraph}>
-        <p>
-        Take the first step towards a fulfilling career and a balanced life. Our personalized coaching sessions empower you to overcome challenges, set clear goals, and achieve meaningful growth.
-        </p>
-      </div>
-      <div><Link to={user !== null ? 'booking' : 'signup'}><button className={styles.ctaButton}>{ user !== null ? 'Book An Interaction ' : 'Transform Your Career!'}</button></Link></div>
+      <Animated y={true} className={styles.ctaText}>
+        <h1 className={styles.ctaHeading}>Unlock Your Potential, Design Your Future</h1>
+        <div className={styles.ctaParagraph}>
+          <p>
+          Take the first step towards a fulfilling career and a balanced life. Our personalized coaching sessions empower you to overcome challenges, set clear goals, and achieve meaningful growth.
+          </p>
+        </div>
+        <div><Link to={user !== null ? 'booking' : 'signup'}><button className={styles.ctaButton}>{ user !== null ? 'Book An Interaction ' : 'Transform Your Career!'}</button></Link></div>
+      </Animated>
     </div>
   </div>
   )
