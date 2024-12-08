d9b6ad4fc15584de06fcb39e1a7dd1eabb06ffb0
Ashade Samson <ashadesamson99@gmail.com>
2024-12-08 10:41:56 +0100

    [style](homepage): Update the about section Restyled the about section to have a more modern feel adding an image to it

--- src/components/landingPage/aboutUs.jsx
+++ src/components/landingPage/aboutUs.jsx
@@ -1,36 +1,41 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from '../../assets/styles/landingPage/aboutUs.module.css'
 import { text } from '../../services/fake-data/data';
 import Animated from '../animations/animated';
-
+import aboutImg from '../../assets/images/about.webp';
+import { Link } from 'react-router-dom';  
 
 function AboutUs() {
 
-  const [isExpanded, setIsExpanded] = useState(false);
   const previewText = text.slice(0, 500);
-
   const paragraphs = text.split('\n').filter(paragraph => paragraph.trim() !== '');
 
-
   return (
-      <div className={styles.aboutContainer}>
-        <Animated y={true} className={styles.aboutContainer}>
-          <h2 className={styles.heading}>About Us</h2>
-          <div className={styles.text}>
-            {isExpanded
-              ? paragraphs.map((paragraph, index) => (
-                  <p key={index} className={styles.paragraph}>
-                    {paragraph}
-                  </p>
-                ))
-              : `${previewText}...`}
+    <div className={styles.aboutContainer}>
+      <Animated y={true} className={styles.aboutContainer}>
+        <h2 className={styles.heading}>About Us</h2>
+
+        <div className={styles.contentWrapper}>
+          <div className={styles.image}>
+            <img src={aboutImg} alt="About Us" className={styles.aboutImage} />
           </div>
-          <button className={styles.toggleButton} onClick={() => setIsExpanded(!isExpanded)}>
-            {isExpanded ? 'Read Less' : 'Read More'}
-          </button>
-        </Animated>
-      </div>
+
+          <div className={styles.text}>
+            {paragraphs.length > 0 ? (
+              <p className={styles.paragraph}>
+                {paragraphs[0]} {paragraphs.length > 1 && '...'}
+              </p>
+            ) : (
+              <p>{previewText}...</p>
+            )}
+            <Link to="about" className={styles.readMoreLink}>
+              Read More
+            </Link>
+          </div>
+        </div>
+      </Animated>
+    </div>
   )
 }
 
-export default AboutUs
+export default AboutUs;
