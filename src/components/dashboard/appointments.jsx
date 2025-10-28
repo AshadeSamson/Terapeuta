8644761490bd2bcba66c9264b8508811119de050
Ashade Samson <ashadesamson99@gmail.com>
2025-10-28 16:16:10 +0100

    [feat]: Add the Loading Compponent for loading fallbacks

--- src/components/dashboard/appointments.jsx
+++ src/components/dashboard/appointments.jsx
@@ -3,6 +3,7 @@ import { useLoaderData, defer, Await } from 'react-router-dom'
 import styles from '../../assets/styles/dashboard/appointments.module.css'
 import appointmentService from '../../services/databaseService/appointmentService'
 import AppointmentsTable from './appointmentsTable'
+import Loading from '../utilities/loading'
 
 
 
@@ -29,7 +30,7 @@ function Appointments() {
     <div className={styles.appointmentsContainer}>
       <>
       <h4>Appointments</h4>
-      <React.Suspense fallback={<h1>Loading appointments, Please wait!</h1>}>
+      <React.Suspense fallback={<Loading text='Fetching your appointments...'/>}>
         <Await resolve={loaderData.appointments}>
           {(loadedAppointments) => {
             const data = loadedAppointments.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
