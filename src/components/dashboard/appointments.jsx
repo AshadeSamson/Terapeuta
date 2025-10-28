6497080f42fdee26454eac684e8b83b4e3b047e2
Ashade Samson <ashadesamson99@gmail.com>
2025-10-28 15:51:44 +0100

    [feat]: Update the appointments component to handle data loading for better UX

--- src/components/dashboard/appointments.jsx
+++ src/components/dashboard/appointments.jsx
@@ -1,12 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { useLoaderData, useNavigation, Link } from 'react-router-dom'
-import { useApp } from '../../context/appContext'
-import Pagination from '../utilities/pagination'
-import { toast } from 'react-toastify'
+import React from 'react'
+import { useLoaderData, defer, Await } from 'react-router-dom'
 import styles from '../../assets/styles/dashboard/appointments.module.css'
-import { IconContext } from 'react-icons'
-import { FaDeleteLeft } from 'react-icons/fa6'
 import appointmentService from '../../services/databaseService/appointmentService'
+import AppointmentsTable from './appointmentsTable'
 
 
 
@@ -18,129 +14,32 @@ export const loader = (userContext) => async () => {
     return null;
   }
 
-  const appointments =  await appointmentService.getAllAppointments(user.uid)
-  const data = appointments.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+  const appointmentsPromise = appointmentService.getAllAppointments(user.uid)
   
-  return data
+  return defer({ appointments: appointmentsPromise});
   
 
 }
 
 function Appointments() {
 
-  const data = useLoaderData();
-  const navigation = useNavigation()
-  const pageIsLoading = navigation.state === "loading";
-
-  const { user } = useApp()
-
-  // Sort appointments by date
-  const [appointments, setAppointments] = useState(data.sort((a, b) => new Date(b.appointmentDate) - new Date(a.appointmentDate)));
-
-  // Pagination
-  const [currentPage, setCurrentPage] = useState(1);
-  const [appointmentsPerPage] = useState(5);
-
-  const indexOfLastAppointment = currentPage * appointmentsPerPage;
-  const indexOfFirstAppointment = indexOfLastAppointment - appointmentsPerPage;
-  const currentAppointments = appointments.slice(indexOfFirstAppointment, indexOfLastAppointment);
-
-  // Change page
-  const paginate = pageNumber => setCurrentPage(pageNumber);
-
-
-  // refresh data
-  async function fetchAppointments() {
-    try {
-      const result = await appointmentService.getAllAppointments(user.uid);
-      const updatedAppointments = result.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-      setAppointments(
-        updatedAppointments.sort((a, b) => new Date(b.appointmentDate) - new Date(a.appointmentDate))
-      );
-    } catch (e) {
-      toast.error(`Failed to fetch appointments`);
-    }
-  }
-
-
-  // Appointment Delete handler
-  async function handleDelete(id) {
-    const confirmDelete = window.confirm("Are you sure you want to cancel this appointment?");
-    if(confirmDelete){
-    try {
-      await appointmentService.deleteAppointment(id);
-      toast.success("Appointment deleted successfully");
-      setAppointments(prev => prev.filter(app => app.id !== id));
-    } catch (e) {
-      toast.warning(`Appointment not deleted: ${e.message}`);
-    }
-  }
-  };
-
-
+  const loaderData = useLoaderData();
 
   return (
     <div className={styles.appointmentsContainer}>
-      { pageIsLoading ? (
-        <div className="loading-screen">
-          <h2>Fetching Appointments...</h2>
-        </div>
-      ) : (
       <>
       <h4>Appointments</h4>
-        {appointments.length > 0 ? (
-          <>
-          <table className={styles.appointmentsTable}>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Client</th>
-                <th>Type</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {currentAppointments.map((appointment) => (
-                <tr key={appointment.id}>
-                  <td>
-                  <Link className={styles.appointmentLinks} to={`/bookings/${appointment.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>{appointment.appointmentDate}
-                  </Link>
-                  </td>
-                  <td>
-                  <Link className={styles.appointmentLinks} to={`/bookings/${appointment.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>{appointment.time}
-                  </Link>
-                  </td>
-                  <td>
-                  <Link className={styles.appointmentLinks} to={`/bookings/${appointment.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>{appointment.name}
-                  </Link>
-                  </td>
-                  <td>
-                  <Link className={styles.appointmentLinks} to={`/bookings/${appointment.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>{appointment.therapyType}
-                  </Link>
-                  </td>
-                  
-                  <td>
-                  <IconContext.Provider value={{size: "2rem"}}>
-                  <a onClick={() => handleDelete(appointment.id)}><FaDeleteLeft className={styles.deleteButton}/></a>
-                  </IconContext.Provider>
+      <React.Suspense fallback={<h1>Loading appointments, Please wait!</h1>}>
+        <Await resolve={loaderData.appointments}>
+          {(loadedAppointments) => {
+            const data = loadedAppointments.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+            
+            return <AppointmentsTable data={data} />
+          }}
 
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <Pagination
-            appointmentsPerPage={appointmentsPerPage}
-            totalAppointments={appointments.length}
-            paginate={paginate}
-          />
-          </>
-        ) : (
-          <h3 className={styles.noBooking}>No Booked Appointments as of Now.</h3>
-        )}
-        </>
-      )}
+        </Await>
+      </React.Suspense>
+      </>
     </div>
   );
 }
